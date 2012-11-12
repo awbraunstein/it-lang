3998c37f6c3f83d3fc595c0ecd48f29db92b6f11
Andrew Braunstein <awbraunstein@gmail.com>
2012-11-12 18:42:58 -0500

    Extended the parser to work with binary expressions and the while loop

--- Parser.hs
+++ Parser.hs
@@ -17,33 +17,41 @@ reserved = P.reserved lexer
 symbol = P.symbol lexer
 semi = P.semi lexer
 semiSep = P.semiSep lexer
+stringLiteral = P.stringLiteral lexer
 
 parseNat = toNat <$> natural
+parseBool = toBool <$> stringLiteral
+
+toBool "true" = True
+toBool "false" = False
 
 toNat 0 = Z
 toNat n = S (toNat (n-1))
 
-parseProg = braces (semiSep parseStmt)
+parseProg = braces $ semiSep parseStmt
 
 parseStmt =
-      Assign <$> identifier <*> (symbol "=" *> parseExp)
-  <|> Block <$> parseProg
-  <|> If <$> parseBExp
-         <*> (reserved "then" *> parseStmt)
-         <*> (reserved "else" *> parseStmt <* reserved "endif")
+  Assign     <$> identifier <*> (symbol "=" *> parseExp)
+  <|> Block  <$> parseProg
+  <|> If     <$> parseBExp
+      <*> (reserved "then" *> parseStmt)
+      <*> (reserved "else" *> parseStmt <* reserved "endif")
   <|> Repeat <$> (reserved "repeat" *> parseExp)
-             <*> (reserved "times" *> parseStmt <* reserved "done")
-
--- parseExp =
---       Lit <$> parseNat
---   <|> V   <$> identifier
---   <|> Plus
+      <*> (reserved "times" *> parseStmt <* reserved "done")
+  <|> While  <$> (reserved "while" *> parseBExp)
+      <*> (reserved "do" *> parseStmt <* reserved "done")
 
 parseExp =
-  Lit <$> parseNat
-  <|> V <$> identifier
-  <|> Plus <$> parseExp <*> (symbol "+" *> parseExp)
+  Lit       <$> parseNat
+  <|> V     <$> identifier
+  <|> Plus  <$> parseExp <*> (symbol "+" *> parseExp)
   <|> Minus <$> parseExp <*> (symbol "-" *> parseExp)
   <|> Times <$> parseExp <*> (symbol "*" *> parseExp)
 
-parseBExp = undefined
+parseBExp =
+  BLit    <$> parseBool
+  <|> Eq  <$> parseExp <*> (symbol "==" *> parseExp)
+  <|> Lt  <$> parseExp <*> (symbol "<" *> parseExp)
+  <|> Not <$> (symbol "!" *> parseBExp)
+  <|> Or  <$> parseBExp <*> (symbol "||" *> parseBExp)
+  <|> And <$> parseBExp <*> (symbol "&&" *> parseBExp)
